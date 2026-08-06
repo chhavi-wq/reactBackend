8b262f877d28ea31ed9806b6eb8cbd4321227faf
apple <apple@apples-MacBook-Air.local>
2026-08-06 15:36:46 +0530

    night

--- controller/controller.js
+++ controller/controller.js
@@ -8,13 +8,11 @@ const nodemailer = require("nodemailer")
 
 const Order = require("../model/orderModel");
 
-
-
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-     host: "smtp.gmail.com",
+    host: "smtp.gmail.com",
     port: 465,
     secure: true,
+    family: 4,
     auth: {
         user: process.env.USER_EMAIL,
         pass: process.env.USER_PASS
