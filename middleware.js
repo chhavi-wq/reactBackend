98e18bed02183c4bf1d27eefae42fd97bf7fd05f
apple <apple@apples-MacBook-Air.local>
2026-08-08 20:54:38 +0530

    new changes

--- middleware.js
+++ middleware.js
@@ -1,30 +1,42 @@
 const jwt = require("jsonwebtoken");
 const Client = require("../model/model");
+
 const middleware = async (req, res, next) => {
-  try {
-   const authHeader = req.headers.authorization;
+    try {
+        const authHeader = req.headers.authorization;
 
-if (!authHeader) {
-    return res.status(401).json({
-        message: "Token not provided"
-    });
-}
-const token = authHeader.split(" ")[1];
+        if (!authHeader) {
+            return res.status(401).json({
+                message: "Token not provided"
+            });
+        }
 
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const token = authHeader.split(" ")[1];
 
-    const user = await Client.findById(decoded.id)
-    if(!user){
-      return res.status(401).json({message:"User no longer exists. Please login again."})
+        const decoded = jwt.verify(
+            token,
+            process.env.JWT_SECRET
+        );
+
+        const user = await Client.findById(decoded.id);
+
+        if (!user) {
+            return res.status(401).json({
+                message: "User no longer exists. Please login again."
+            });
+        }
+
+        req.user = decoded;
+
+        next();
+
+    } catch (error) {
+        console.error("AUTH ERROR:", error);
+
+        return res.status(401).json({
+            message: "Unauthorized! Invalid or Expired Token"
+        });
     }
-    req.user = decoded; 
-
-    next();
-  } catch (error) {
-    return res.status(401).json({
-      message: "Unauthorized! Invalid or Expired Token",
-    });
-  }
 };
 
 module.exports = middleware;
